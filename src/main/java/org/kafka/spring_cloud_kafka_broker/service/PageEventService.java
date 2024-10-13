7d2a23920f4245d5f3e5f0dec5033e3319dec8d7
YounesJabbour <younes0jabbour@gmail.com>
2024-10-13 22:12:06 +0100

    add spring kafka stream function for stream processing

--- src/main/java/org/kafka/spring_cloud_kafka_broker/service/PageEventService.java
+++ src/main/java/org/kafka/spring_cloud_kafka_broker/service/PageEventService.java
@@ -4,10 +4,13 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.kafka.spring_cloud_kafka_broker.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -37,11 +40,26 @@ public class PageEventService {
                     .build();
         };
     }
-    @Bean
+//    @Bean
     public Function<PageEvent, PageEvent> pageEventFunction() {
         return (pageEvent -> {
             pageEvent.setName("Name Changed");
             return pageEvent;
         });
      }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction() {
+        return input -> input
+                .filter((k, v) -> v.getDuration() > 100)
+                .map((k, v) -> new KeyValue<>(v.getName(), 0L))
+                .groupBy((k, v) -> k, Grouped.with(Serdes.String(), Serdes.Long()))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                .count(Materialized.as("counts-store"))
+                .toStream()
+                .map((k, v) -> new KeyValue<>(
+                        "Window: [" + k.window().startTime() + " - " + k.window().endTime() + "] Key: " + k.key(), v
+                ));
+    }
+
 }
